e884422cb207f592ce9f11a5c8420b9bd9490ba7
max <sobolmaxim@gmail.com>
2023-11-20 22:46:45 +0200

    thunk library

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
+
 import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const rootElement = document.getElementById("root");
+
+render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -18,5 +20,6 @@ root.render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,32 +1,40 @@
-import { useState, useEffect, Fragment } from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import ProductCard from "../../components/product-card/product-card.component";
+import { useState, useEffect, Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
+import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
-import "./category.styles.scss";
+import {
+  selectCategoriesMap,
+  selectIsLoading,
+} from '../../store/categories/categories.selector';
+
+import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
-  console.log("render/re-rendering category component");
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
-    console.log("effect fired calling setProducts");
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
   return (
     <Fragment>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </div>
+      <Title>{category.toUpperCase()}</Title>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 };
